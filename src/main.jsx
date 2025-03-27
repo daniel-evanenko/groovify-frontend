6b2bbce27cbe44c69e7fc6698beeb38ebe46573c
daniel <daniel.evanenko@gmail.com>
2025-03-27 20:19:34 +0200

    add basic scss

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,11 @@
 import { createRoot } from 'react-dom/client'
-import './index.scss'
+import './assets/styles/main.scss'
 import App from './App.jsx'
-
+import { store } from './store/store.js'
+import { Provider } from 'react-redux'
 createRoot(document.getElementById('root')).render(
-  <App />
+  <Provider store={store}>
+    <App />
+  </Provider>
+
 )
