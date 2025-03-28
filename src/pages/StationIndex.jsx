f2629dae11e43eaf304d4d4821fc3e1f4ad3f7e9
TheSkyOne <skyscroll.dev@gmail.com>
2025-03-28 23:25:24 +0300

    StationList displays previews (#11)

--- src/pages/StationIndex.jsx
+++ src/pages/StationIndex.jsx
@@ -1,8 +1,11 @@
+import { StationList } from "../cmps/StationList";
+
 export function StationIndex() {
 
     return (
         <section className="station-index">
             <h1>StationIndex</h1>
+            <StationList></StationList>
         </section>
     )
 }
