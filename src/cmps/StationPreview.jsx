1c43c08a8250dfdef840b6b2077cde7d97bb8ba9
TheSkyOne <42645782+TheSkyOne@users.noreply.github.com>
2025-03-28 23:29:33 +0300

    added  click functionality to station preview

--- src/cmps/StationPreview.jsx
+++ src/cmps/StationPreview.jsx
@@ -1,12 +1,14 @@
 import { LongTxt } from "./LongTxt"
+import { useNavigate } from "react-router-dom"
 import tmpImg from "/tmp-assets/temp_station_img.jpg"
 
-export function StationPreview({ imgSrc = tmpImg, desc = "lorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsum" }) {
-    
+export function StationPreview({ station }) {
+    const navigate = useNavigate()
+
     return (
-        <article className="station-preview">
-            <img src={imgSrc} alt="station-img"></img>
-            <LongTxt>{desc}</LongTxt>
+        <article className="station-preview" onClick={() => navigate(`/station/${station?._id}`)}>
+            <img src={station?.img || tmpImg} alt="station-img"></img>
+            <LongTxt>{station?.desc || "loremipsum"}</LongTxt>
         </article>
     )
 }
