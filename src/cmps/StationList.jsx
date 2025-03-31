2fb078ca44cac12906116152fb044bf7efde8563
TheSkyOne <skyscroll.dev@gmail.com>
2025-03-31 18:35:41 +0300

    Revert "Omer/station index display (#17)"
    This reverts commit 983f94dd9a9819c0978e05dc198d67d3cc70e09c.

--- src/cmps/StationList.jsx
+++ src/cmps/StationList.jsx
@@ -1,31 +1,16 @@
-import { useEffect, useRef, useState } from "react"
 import { StationPreview } from "./StationPreview"
 
 export function StationList({ stations }) {
-    const elStationsList = useRef()
-    const [displayStationsAmount, setDisplayStationsAmount] = useState(stations.length)
-
-
-    useEffect(() => {
-        const resizeObserver = new ResizeObserver(entries => {
-            const stationListWidth = entries[0].contentRect.width
-            const displayAmount = Math.max(Math.floor(stationListWidth / 200), 3)
-            setDisplayStationsAmount(displayAmount)
-
-        })
-
-        resizeObserver.observe(elStationsList.current)
-        return () => resizeObserver.disconnect()
-    }, [])
 
     const listTitle = stations[0].category
     let a = 1
+
     return (
-        <article ref={elStationsList} className="station-list">
+        <article className="station-list">
             <a>{listTitle}</a>
             <div className="station-previews">
-                {stations.slice(0, displayStationsAmount).map(station =>
-                    <StationPreview key={station.spotifyId + `${a++}`} station={station}></StationPreview>
+                {stations.map(station =>
+                    <StationPreview key={station.spotifyId+`${a++}`} station={station}></StationPreview>
                 )}
             </div>
         </article>
