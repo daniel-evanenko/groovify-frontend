0974182ccd549e05e0c3334562d079ba04d21e08
TheSkyOne <42645782+TheSkyOne@users.noreply.github.com>
2025-03-31 18:34:00 +0300

    dynamically display Stations based on width

--- src/cmps/LongTxt.jsx
+++ src/cmps/LongTxt.jsx
@@ -3,5 +3,5 @@ export function LongTxt({ children, length = 45 }) {
     if (children.length < length) displayText = children
     else displayText = children.slice(0, length) + "..."
 
-    return <p className="long-txt">{displayText}</p>
+    return <p dir="auto" className="long-txt">{displayText}</p>
 }
--- src/cmps/StationList.jsx
+++ src/cmps/StationList.jsx
@@ -1,16 +1,31 @@
+import { useEffect, useRef, useState } from "react"
 import { StationPreview } from "./StationPreview"
 
 export function StationList({ stations }) {
+    const elStationsList = useRef()
+    const [displayStationsAmount, setDisplayStationsAmount] = useState(stations.length)
+
+
+    useEffect(() => {
+        const resizeObserver = new ResizeObserver(entries => {
+            const stationListWidth = entries[0].contentRect.width
+            const displayAmount = Math.max(Math.floor(stationListWidth / 200), 3)
+            setDisplayStationsAmount(displayAmount)
+
+        })
+
+        resizeObserver.observe(elStationsList.current)
+        return () => resizeObserver.disconnect()
+    }, [])
 
     const listTitle = stations[0].category
     let a = 1
-
     return (
-        <article className="station-list">
+        <article ref={elStationsList} className="station-list">
             <a>{listTitle}</a>
             <div className="station-previews">
-                {stations.slice(0, 9).map(station =>
-                    <StationPreview key={station.spotifyId+`${a++}`} station={station}></StationPreview>
+                {stations.slice(0, displayStationsAmount).map(station =>
+                    <StationPreview key={station.spotifyId + `${a++}`} station={station}></StationPreview>
                 )}
             </div>
         </article>
