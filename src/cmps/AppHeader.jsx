21bd72497446dd16a2059f8b130a718a02c396e7
TheSkyOne <skyscroll.dev@gmail.com>
2025-03-31 20:31:33 +0300

    Omer/app header styling (#20)
    * refined header styling
    
    * clicking on home button or app icon navigates to StationIndex

--- src/cmps/AppHeader.jsx
+++ src/cmps/AppHeader.jsx
@@ -1,10 +1,14 @@
-import { useRef, useState } from "react"
+import { useNavigate } from "react-router-dom"
 import icon from "/icons/favicon.ico"
+import { useRef, useState } from "react"
 
 export function AppHeader() {
     const [fillHome, setFillHome] = useState(true)
     const [fillBrowse, setFillBrowse] = useState(false)
     const elSearchOptions = useRef()
+    const elSearchButton = useRef()
+    const elSearchInput = useRef()
+    const navigate = useNavigate()
 
     function onInputFocus() {
         elSearchOptions.current.classList.add("focused")
@@ -12,11 +16,18 @@ export function AppHeader() {
 
     function onInputFocusLost() {
         elSearchOptions.current.classList.remove("focused")
+        elSearchButton.current.style.setProperty("cursor", "pointer")
     }
 
     function onHomeClicked() {
         setFillHome(true)
         setFillBrowse(false)
+        navigate("/")
+    }
+
+    function onSearchClicked() {
+        elSearchInput.current.focus()
+        elSearchButton.current.style.setProperty("cursor", "default")
     }
 
     function onBrowseClicked() {
@@ -24,7 +35,7 @@ export function AppHeader() {
         setFillHome(false)
     }
 
-    
+
     const homeEmpty =
         <svg xmlns="http://www.w3.org/2000/svg" data-encore-id="icon" role="img" aria-hidden="true" className="e-9640-icon" viewBox="0 0 24 24">
             <path d="M12.5 3.247a1 1 0 0 0-1 0L4 7.577V20h4.5v-6a1 1 0 0 1 1-1h5a1 1 0 0 1 1 1v6H20V7.577l-7.5-4.33zm-2-1.732a3 3 0 0 1 3 0l7.5 4.33a2 2 0 0 1 1 1.732V21a1 1 0 0 1-1 1h-6.5a1 1 0 0 1-1-1v-6h-3v6a1 1 0 0 1-1 1H3a1 1 0 0 1-1-1V7.577a2 2 0 0 1 1-1.732l7.5-4.33z" fill="currentColor" />
@@ -48,7 +59,7 @@ export function AppHeader() {
 
     return (
         <header className="app-header">
-            <img className="icon" src={icon} alt="icon"></img>
+            <img className="icon" src={icon} alt="icon" onClick={onHomeClicked} style={{cursor: "pointer"}}></img>
 
             <div className="nav-options">
                 <button className="home-btn nav-btn" onClick={onHomeClicked}>
@@ -56,13 +67,14 @@ export function AppHeader() {
                 </button>
 
                 <div className="search-options" ref={elSearchOptions}>
-                    <button className="search-btn nav-btn">
+                    <button ref={elSearchButton} className="search-btn nav-btn" onClick={onSearchClicked}>
                         <svg xmlns="http://www.w3.org/2000/svg" data-encore-id="icon" role="img" aria-hidden="true" className="e-9640-icon" viewBox="0 0 24 24">
                             <path d="M10.533 1.27893C5.35215 1.27893 1.12598 5.41887 1.12598 10.5579C1.12598 15.697 5.35215 19.8369 10.533 19.8369C12.767 19.8369 14.8235 19.0671 16.4402 17.7794L20.7929 22.132C21.1834 22.5226 21.8166 22.5226 22.2071 22.132C22.5976 21.7415 22.5976 21.1083 22.2071 20.7178L17.8634 16.3741C19.1616 14.7849 19.94 12.7634 19.94 10.5579C19.94 5.41887 15.7138 1.27893 10.533 1.27893ZM3.12598 10.5579C3.12598 6.55226 6.42768 3.27893 10.533 3.27893C14.6383 3.27893 17.94 6.55226 17.94 10.5579C17.94 14.5636 14.6383 17.8369 10.533 17.8369C6.42768 17.8369 3.12598 14.5636 3.12598 10.5579Z" fill="currentColor" />
                         </svg>
                     </button>
 
                     <input
+                        ref={elSearchInput}
                         className="search-input"
                         type="text"
                         placeholder="What do you want to play?"
