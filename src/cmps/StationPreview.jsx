1537a216d30e39c33db0ce4d914df0b1cd516e8a
TheSkyOne <42645782+TheSkyOne@users.noreply.github.com>
2025-04-03 16:38:46 +0300

    added play button to StationPreview with styling and animation

--- src/cmps/StationPreview.jsx
+++ src/cmps/StationPreview.jsx
@@ -2,6 +2,7 @@ import { LongTxt } from "./LongTxt"
 import { useNavigate } from "react-router-dom"
 import tmpImg from "/tmp-assets/temp_station_img.jpg"
 import { setActiveStation } from "../store/actions/library.actions"
+import { PlayButton } from "./PlayButton"
 
 export function StationPreview({ station }) {
     const navigate = useNavigate()
@@ -17,7 +18,10 @@ export function StationPreview({ station }) {
 
     return (
         <article className="station-preview" onClick={handleStationClick}>
-            <img src={station?.imgUrl || tmpImg} alt="station-img"></img>
+            <div>
+                <img src={station?.imgUrl || tmpImg} alt="station-img"></img>
+                <PlayButton />
+            </div>
             <LongTxt>{station?.description || "Your weekly update of the most played tracks right now - Global."}</LongTxt>
         </article>
     )
