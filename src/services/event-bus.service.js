85e9c85db759c9c1d2dd1c11b5eec1adfa933157
TheSkyOne <42645782+TheSkyOne@users.noreply.github.com>
2025-04-04 19:11:21 +0300

    farther refinement of styling and more percise functionality

---
+++ src/services/event-bus.service.js
@@ -0,0 +1,20 @@
+//events
+export const INDEX_MOUNT = "on-index-mount"
+
+function createEventEmitter() {
+    const listenersMap = {}
+    return {
+        on(evName, listener) {
+            listenersMap[evName] = (listenersMap[evName]) ? [...listenersMap[evName], listener] : [listener]
+            return () => {
+                listenersMap[evName] = listenersMap[evName].filter(func => func !== listener)
+            }
+        },
+        emit(evName, data) {
+            if (!listenersMap[evName]) return
+            listenersMap[evName].forEach(listener => listener(data))
+        }
+    }
+}
+
+export const eventBus = createEventEmitter()
