c9e840ebc669f45a05435926d88ccab96867a146
TheSkyOne <42645782+TheSkyOne@users.noreply.github.com>
2025-05-18 00:11:25 +0300

    change save method to local storage instead of file based

--- src/services/spotify/spotify-api.service.js
+++ src/services/spotify/spotify-api.service.js
@@ -3,21 +3,25 @@ import { getSpotifyToken } from "./spotify-token.service.js";
 import { loadFromStorage, saveToStorage } from "../util.service.js";
 
 const STORAGE_KEY = "categories"
-const accessToken = await getSpotifyToken()
 
 export async function getCategories() {
+    console.log("trying to get categories")
     let categories = loadFromStorage(STORAGE_KEY)
     try {
         if (categories && categories.length > 0) return categories
 
-        categories = await axios.get("https://api.spotify.com/v1/browse/categories", {
+        const accessToken = await getSpotifyToken()
+
+        const response = await axios.get("https://api.spotify.com/v1/browse/categories", {
             headers: {
                 Authorization: `Bearer ${accessToken}`
             }
         })
 
-        saveToStorage(STORAGE_KEY, categories)
-        return categories.data.categories.items
+        const items = response.data.categories.items
+
+        saveToStorage(STORAGE_KEY, items)
+        return items
 
     } catch (err) {
         console.error(err)
