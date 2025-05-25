de7ef9016bd68c44b427ffb97d45e43326429ee8
TheSkyOne <42645782+TheSkyOne@users.noreply.github.com>
2025-05-25 18:13:28 +0300

    fix improper background color name in category preview

--- client/src/cmps/CategoryPreview.jsx
+++ client/src/cmps/CategoryPreview.jsx
@@ -1,7 +1,7 @@
 export function CategoryPreview({ category }) {
 
     const colors = [
-        "rgb(220, 20, 140)", "rgb(0, 100, 80)", "rgb(132, 0, 231)","rgb(96, 129, 8)",
+        "rgb(220, 20, 140)", "rgb(0, 100, 80)", "rgb(132, 0, 231)", "rgb(96, 129, 8)",
         "rgb(71, 125, 149)", "rgb(13, 115, 236)", "rgb(80, 55, 80)", "rgb(119, 119, 119)",
         "rgb(175, 40, 150)", "rgb(141, 103, 171)", "rgb(233, 20, 41)", "rgb(30, 50, 100)",
         "rgb(216, 64, 0)", "rgb(186, 93, 7)", "rgb(176, 98, 57)", "rgb(140, 25, 50)",
@@ -10,7 +10,7 @@ export function CategoryPreview({ category }) {
     const randColor = colors[Math.floor(Math.random() * colors.length)]
 
     return (
-        <article className="category-preview" style={{backgroundColor: randColor}}>
+        <article className="category-preview" style={{ backgroundColor: randColor }}>
             <span>{category.name}</span>
             <img src={`${category.icons[0].url}`}></img>
         </article>
