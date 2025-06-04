e5db8bc9493edff938093e7436208cb7d68d534d
TheSkyOne <42645782+TheSkyOne@users.noreply.github.com>
2025-06-04 21:38:53 +0300

    remove extra calls to backend

--- client/src/cmps/StationPreview.jsx
+++ client/src/cmps/StationPreview.jsx
@@ -2,8 +2,6 @@ import { LongTxt } from "./LongTxt"
 import { useNavigate } from "react-router-dom"
 import defaultImg from "/img/default-playlist-img.png"
 import { PlayButton } from "./PlayButton"
-import { loadStation } from "../store/actions/station.actions"
-import { stationService } from "../services/station/station.service"
 
 export function StationPreview({ station }) {
     const navigate = useNavigate()
@@ -14,23 +12,7 @@ export function StationPreview({ station }) {
             return
         }
 
-        else if (!station._id) {
-            try {
-                const newStation = await stationService.getById(station.id)
-                loadStation(newStation._id)
-                navigate(`/station/${newStation._id}`)
-                return
-            } catch (err) {
-                console.error("error getting new station from spotify", err)
-                navigate('/')
-                throw err
-            }
-        }
-
-        else {
-            loadStation(station._id)
-            navigate(`/station/${station._id}`)
-        }
+        navigate(`/station/${station._id}`)
     }
 
     const imgUrl = station?.images[0]?.url || defaultImg
