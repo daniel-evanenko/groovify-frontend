30217b1e286ef6704b42fb61d5f77432a152fbaa
gelbardavid <44742728+gelbardavid@users.noreply.github.com>
2025-06-04 19:56:21 +0300

    public removal string (#105)

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,5 +1,5 @@
 
 export const DEFAULT_PORT = 3000;
-export const DEFAULT_IMAGE_URL = '/public/img/default-playlist-img.png';
+export const DEFAULT_IMAGE_URL = '/img/default-playlist-img.png';
 export const INITIAL_STATION_PREFIX = "My Station #";
 export const INITIAL_STATION_PREFIX_REGEX = /[a-zA-Z #]/g;
